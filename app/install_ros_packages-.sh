b16909b1d4ba1516f05e541875a70582c8ec5e7a
foqhn <haraharakobuchi0911@gmail.com>
2025-11-26 17:38:35 +0900

    add

---
+++ app/install_ros_packages-.sh
@@ -0,0 +1,4 @@
+sudo apt install python3-dev python3-pip libfreetype6-dev libjpeg-dev build-essential libopenjp2-7 libtiff5
+sudo apt install liblgpio-dev
+sudo apt install ros-humble-cv-bridge
+sudo apt install ros-humble-camera-ros
